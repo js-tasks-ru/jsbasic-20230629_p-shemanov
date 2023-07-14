35ba430333dcb9894c91f6f8ae6e6b169ee9bb66
Pavel Shemanov <p.shemanovich@gmail.com>
2023-07-14 09:57:14 +0300

    Задачи из раздела 3 по массивам и функциям решены!

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let resultStr = '';
+
+  for(user of users){
+    if(user.age <= age){
+      resultStr += resultStr === '' 
+                ? `${user.name}, ${user.balance}` 
+                : `\n${user.name}, ${user.balance}`;
+    }
+  }
+
+  return resultStr;
 }
