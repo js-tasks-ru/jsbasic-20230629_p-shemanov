dd7cb5fe6c58d3463b4265b2b513834f36959d54
Pavel Shemanov <p.shemanovich@gmail.com>
2023-07-27 23:18:24 +0300

    Задачи модуля 5 сделаны!

---
+++ 5-module/3-task/index.js
@@ -0,0 +1,36 @@
+function initCarousel() {
+  let rightArrow = document.querySelector('.carousel__arrow_right');
+  let leftArrow = document.querySelector('.carousel__arrow_left');
+  let arrows = document.querySelectorAll('.carousel__arrow');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let counter = 0;
+  let slideWidth = document.querySelector('.carousel').offsetWidth;
+
+  leftArrow.style.display = 'none';
+
+  rightArrow.addEventListener('click', function(){
+    counter++;
+    let shift = 'translateX(-' + counter * slideWidth + 'px)';
+    carouselInner.style.transform = shift;
+  });
+
+  leftArrow.addEventListener('click', function(){
+    --counter;
+    let shift = 'translateX(-' + counter * slideWidth + 'px)';
+    carouselInner.style.transform = shift;
+  });
+
+  arrows.forEach((arrow) => {
+    arrow.addEventListener('click', function(){
+      if(counter === 0){
+        leftArrow.style.display = 'none';
+      } else if(counter < carouselInner.children.length - 1){
+        rightArrow.style.display = 'flex';
+        leftArrow.style.display = 'flex';
+      }
+      else{
+        rightArrow.style.display = 'none'
+      }
+    })
+  });
+}
